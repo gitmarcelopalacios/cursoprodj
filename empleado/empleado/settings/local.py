e2d692b3145ccccbdb1aa2caf10d3eb4918889ed
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2022-07-29 15:08:01 -0300

    primero

--- empleado/empleado/settings/local.py
+++ empleado/empleado/settings/local.py
@@ -8,10 +8,22 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DEFINICION PARA POSTGRESQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',
+        'USER': 'marce',
+        'PASSWORD': 'marce',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
