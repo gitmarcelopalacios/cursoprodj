33503c32c6410f747eb9831e2468040c9010d9b8
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2022-07-28 21:29:59 -0300

    models

--- empleado/applications/departamento/models.py
+++ empleado/applications/departamento/models.py
@@ -6,6 +6,5 @@ class Departamento(models.Model):
     name = models.CharField('Nombre',max_length=50)
     shor_name = models.CharField('Nombre Corto',max_length=20)
     anulate = models.BooleanField()
-
     def __str__(self):
         return str(self.id)+self.name+'-'+self.shor_name
