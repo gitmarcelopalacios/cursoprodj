a7097cf0f633ae167fb528632eec3dbb20cdd393
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2022-08-01 09:35:29 -0300

    47

--- empleado/applications/persona/urls.py
+++ empleado/applications/persona/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('lista-by-area/<shorname>/', views.ListByAreaEmpleado.as_view()),
     path('buscar-empleado/', views.ListEmpleadosByKword.as_view()),
     path('lista-habilidades-empleado/', views.ListHabilidadesEmpleados.as_view()),
+    path('ver-empleado/<pk>/', views.EmpleadoDetailView.as_view()),
+    path('add-empleado/', views.EmpleadoCreateView.as_view()),
+    path('success/', views.SuccessView.as_view()),
 ]
--- empleado/applications/persona/views.py
+++ empleado/applications/persona/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 # importo la clase
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView 
 
 # importo el modelo
 from .models import Empleado
@@ -49,7 +49,28 @@ class ListHabilidadesEmpleados(ListView):
         empleado = Empleado.objects.get(id=1)
         return empleado.habilidades.all()
 
+class EmpleadoDetailView(DetailView):
+    model = Empleado
+    template_name = "persona/detail_empleado.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(EmpleadoDetailView, self).get_context_data(**kwargs)
+        context ['titulo'] = 'Empleado del mes'
+        return context
 
 
+class SuccessView(TemplateView):
+    template_name = "persona/success.html"
 
-# listar empleados por trabajo
+
+class EmpleadoCreateView(CreateView):
+    template_name = "persona/add.html"
+    model = Empleado
+    #fields = ['first_name','last_name','job']
+    # muestra todos los campos
+    fields = ('__all__')
+    # recarga la misma pagina
+    #success_url = '.'
+    # redirecciono
+    success_url = '/success'
+
